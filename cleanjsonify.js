ad7504a734cbd2ec51e7b2cfc87b95e3ba2bf705
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2019-09-08 19:42:52 +0530

    Added IITC export cleaner as script

---
+++ cleanjsonify.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+// Run to format and sort the exported IITC-pogo.json file
+// Automatically reads `./IITC-pogo.json` and writes to `./IITC-pogo.json`
+
+const JSON = './IITC-pogo.json'
+
+const writeJsonFile = require('write-json-file')
+const data = require(JSON)
+
+writeJsonFile(JSON, data, {
+  indent: '  ',
+  sortKeys: true,
+}).then(() => console.log('JSON cleaned'))
